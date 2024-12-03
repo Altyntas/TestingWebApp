fb29e8e509eace1cb54543dc8711514def84627f
Altintas <reyaah@rambler.ru>
2024-12-03 17:20:19 +0500

    added table creation and data upload

--- DataWorker/DataMapping/DataMap.cs
+++ DataWorker/DataMapping/DataMap.cs
@@ -45,18 +45,49 @@ namespace DataWorker.DataMapping
                     columns.Add(new DataSetColumns()
                     {
                         Name = sheet.Cells[1, i].Text,
-                        Type = "string" //TODO: Need to develop types parcing
+                        Type = "VARCHAR(50)" //TODO: Need to create type parcing
                     });
                 }
 
-                var tableName = $"{excelFile.Name}_{DateTime.Now}";
+                var tableName = excelFile.Name.Split('.')[0];
+                //TODO: create scheme for users datasets
                 var table = new DataSetTable()
                 {
-                    Name = $"{excelFile.Name}_{DateTime.Now.ToString("yyyy_MM_dd")}",
+                    Name = $"{tableName}_{DateTime.Now.ToString("yyyy_MM_dd")}",
                     Columns = columns,
                     CreateDate = DateTime.Now,
                     UpdateDate = DateTime.Now
                 };
+
+                //Creating table with columns and data type
+                //await _context.Database.ExecuteSqlRawAsync($"CREATE TABLE dbo.{table.Name} ");
+                var createTableSqlString = $"CREATE TABLE dbo.{table.Name} (";
+                for (int i = 0; i < table.Columns.Count(); i++)
+                {
+                    var col = table.Columns.ToList()[i]; //SIC!
+                    createTableSqlString += $"{col.Name} {col.Type}";
+
+                    if (i < table.Columns.Count() - 1)
+                        createTableSqlString += ", ";
+                } 
+                
+                createTableSqlString += ");";
+                await _context.Database.ExecuteSqlRawAsync(createTableSqlString);
+
+
+                for (int j = 1; j < sheet.Rows.Count() + 1; j++)
+                {
+                    if (j == 1) continue; //ignore first row
+
+                    var rowValues = new List<string>();
+
+                    for (int i = 1; i < sheet.Columns.Count() + 1; i++)
+                        rowValues.Add(sheet.Cells[2, i].Text);
+
+                    var insertString = $"INSERT INTO dbo.{table.Name}({string.Join(',', table.Columns.Select(c => c.Name))}) VALUES({string.Join(',', rowValues.Select(r => "\'" + r + "\'"))})";
+                    await _context.Database.ExecuteSqlRawAsync(insertString);
+                }
+                
 
                 return table;
             }
